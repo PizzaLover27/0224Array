c6de7552368eee04e3187687bc97ce8600f450bd
Havasi <havasi.mate@simonyiszki.org>
2025-02-24 13:08:30 +0100

    Tömbök 1D

---
+++ src/tombok_listak1/Tombok_listak1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tombok_listak1;
+
+/**
+ *
+ * @author User
+ */
+public class Tombok_listak1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
